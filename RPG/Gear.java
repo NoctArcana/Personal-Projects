dde0acb3ba6d07025c091874c41e1946422a9539
Biennico Ross Abitria <111473226+NoctArcana@users.noreply.github.com>
2022-09-01 10:03:58 +0800

    Add files via upload

---
+++ RPG/Gear.java
@@ -0,0 +1,14 @@
+public class Gear {
+    
+    int defense;
+    float weight;
+    String name;
+    String description;
+    String rarity;
+    boolean hasElementalResistance;
+
+    public void showGearDescription() {
+        System.out.println(description);
+    }
+
+}
---
+++ RPG/Weaponries.java
@@ -0,0 +1,13 @@
+public class Weaponries {
+
+    int damage;
+    String name;
+    String description;
+    String rarity;
+    Boolean hasElementalDamage;
+    float weight;
+
+    public void showWeaponriesDescription() {
+        System.out.println(description);
+    }
+}
